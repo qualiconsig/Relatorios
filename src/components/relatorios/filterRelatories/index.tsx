91680742dfd799d8623f41b6ed65c8fbc448d4ed
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-03-11 09:25:18 -0300

    fix responsive styles

--- src/components/relatorios/filterRelatories/index.tsx
+++ src/components/relatorios/filterRelatories/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, List, ListItem, UnorderedList } from "@chakra-ui/react";
+import { Box, Flex, List, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import { ListItemCom } from "./list";
 import { useEffect, useState } from "react";
 
@@ -18,52 +18,36 @@ export const FilterDatas = ({selectedMonth}: any) => {
         w={"50vw"}
         margin={"0 auto"}
       >
-        <Flex bg={"#F4F4F4"} borderRadius={14}>
+        <Flex bg={"#F4F4F4"}   borderRadius={14}>
           <Flex 
-            h={"50vh"}
+            h={{base:"100vh", md:"70vh", lg:"50vh"}}
             justify={"space-around"}
-            w={"60vw"}
-            pr={"20px"}
+            w={{base:"100vw", md:"70vw", lg:"60vw"}}
+            pr={{base:"0px",md:"20px"}}
             color={"black"}
             align={'center'}
           >
             <Box>
-              <UnorderedList listStyleType={"none"} >
-                <ListItemCom value={"Data"}/>
-                <ListItemCom value={"Logados"}/>
-                <ListItemCom value={"Vendas Totais"}/>
-                <ListItemCom value={"Enviados Cip"}/>
-                <ListItemCom value={"%Cip"}/>
-              </UnorderedList>
+              
+                <ListItemCom value={"Data"}  rest={"green"} secondValue={selectedMonth}/>
+                <ListItemCom value={"Logados"} secondValue={"176"}/>
+                <ListItemCom value={"Vendas Totais"} secondValue={"199.585.900"}/>
+                <ListItemCom value={"Enviados Cip"} secondValue={"79.530.200"}/>
+                <ListItemCom value={"%Cip"} secondValue={"55%"}/>
+             
             </Box>
-            <Box>
-              <UnorderedList listStyleType={"none"}>
-                <ListItemCom value={selectedMonth} rest="#06877F"/>
-                <ListItemCom value={"176"}/>
-                <ListItemCom value={"199.585.900"}/>
-                <ListItemCom value={"79.530.200"}/>
-                <ListItemCom value={"55%"}/>
-              </UnorderedList>
-            </Box>
+            
 
             <Box ml={20}>
-              <UnorderedList listStyleType={"none"}>
-                <ListItemCom value={"Cip Paga"}/>
-                <ListItemCom value={"Integrado"}/>
-                <ListItemCom value={"Por consultor"}/>
-                <ListItemCom value={"%Efetivado Cip"}/>
-                <ListItemCom value={"Efetivado Total"}/>
-              </UnorderedList>
+            
+                <ListItemCom value={"Cip Paga"} secondValue={"48.143.105"}/>
+                <ListItemCom value={"Integrado"} secondValue={"55.650.100"}/>
+                <ListItemCom value={"Por consultor"} secondValue={"310.541"}/>
+                <ListItemCom value={"%Efetivado Cip"} secondValue={"58%"}/>
+                <ListItemCom value={"Efetivado Total"} secondValue={"33%"}/>
+          
             </Box>
-            <Box>
-              <UnorderedList listStyleType={"none"}>
-                <ListItemCom value={"48.143.105"}/>
-                <ListItemCom value={"55.650.100"}/>
-                <ListItemCom value={"310.541"}/>
-                <ListItemCom value={"58%"}/>
-                <ListItemCom value={"33%"}/>
-              </UnorderedList>
-            </Box>
+        
           </Flex>
           <Flex>
 
--- src/components/relatorios/filterRelatories/list.tsx
+++ src/components/relatorios/filterRelatories/list.tsx
@@ -1,8 +1,11 @@
-import { ListItem } from "@chakra-ui/react"
+import { Flex, ListItem, Text } from "@chakra-ui/react"
 
-export const ListItemCom = ({value, rest}:any) => {
+export const ListItemCom = ({value, rest, secondValue}:any) => {
 
   return (
-    <ListItem mb={'25px'} border={'1px solid #E4E4E4'} p={'10px'} borderRadius={'7px'} color={rest}>{value}</ListItem>
+    <Flex mb={'25px'} border={'1px solid #E4E4E4'} p={'10px'} borderRadius={'7px'} color={rest} >
+        <Text w={{ md:"10vw", lg:"15vw"}} p={'2px'} color={'black'}>{value}</Text>
+        <Text >{secondValue}</Text>
+    </Flex>
   )
 }
